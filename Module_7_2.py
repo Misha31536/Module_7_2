181d06ec62a53884b851744df2452620d5607741
Misha31536 <e4ik@ya.ru>
2024-08-02 11:55:02 +0700

    2024/08/02 initiate

---
+++ Module_7_2.py
@@ -0,0 +1,47 @@
+import os.path
+from pprint import pprint
+
+def custom_write(file_name, strings):
+    if os.path.isfile(file_name):
+        for i in strings:
+            file = open(file_name, 'r')
+            line = file.read().splitlines()
+            if i in line:
+                file.close()
+                continue
+            else:
+                file = open(file_name, 'a')
+                file.write(i + '\n')
+                file.close()
+        file = open(file_name, 'r')
+        j = 1
+        strings_positions = {}
+        len_ = len(file.readlines())
+        file.close()
+        file = open(file_name, 'r')
+        while j != len_ + 1 :
+            line = file.readline()
+            strings_positions[(j, file.tell())] = line.rstrip('\n')
+            j += 1
+        file.close()
+        return strings_positions
+        # for i in range(len(file.readlines())):
+        #     file = open(file_name, 'r')
+        #     line = file.readline()
+        #     strings_positions[(i, file.tell())] = line.rstrip('\n')
+        #     file.close()
+        # return strings_positions
+    else:
+        for i in strings:
+            file = open(file_name, 'w')
+            file.close()
+            custom_write(file_name, strings)
+
+a = custom_write( "Module_7_2.txt",  [
+    'Text for tell.',
+    'Используйте кодировку utf-8.',
+    'Because there are 2 languages!',
+    'Спасибо!'
+    ])
+print(a)
+
